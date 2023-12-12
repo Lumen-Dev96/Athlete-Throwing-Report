da493508dfa22f830da8165936f922fabba99335
Lumen <lumen@slingshot.sh>
2023-12-12 17:47:10 +0800

    Initial project

---
+++ src/App.vue
@@ -0,0 +1,83 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <img alt="logo1" class="logo" src="@/assets/logo.jpeg" width="80" height="80" />
+
+    <img alt="logo2" class="logo" src="@/assets/logo2.jpeg" width="80" height="90" />
+
+    <div class="wrapper">
+
+      
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  place-items: center;
+  padding-right: calc(var(--section-gap) / 2);
+  padding-left: calc(var(--section-gap) / 2);
+  line-height: 1.5;
+  max-height: 10vh;
+  box-shadow:
+  0px 0.3px 0.5px rgba(0, 0, 0, 0.01),
+  0px 0.8px 1.5px rgba(0, 0, 0, 0.015),
+  0px 1.8px 3.6px rgba(0, 0, 0, 0.02),
+  0px 6px 12px rgba(0, 0, 0, 0.03)
+;
+}
+
+.logo {
+  display: block;
+  margin: 0 2rem 0 0;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,144 @@
+<script setup>
+import { ClockCircleFilled, EnvironmentFilled, EyeFilled } from '@ant-design/icons-vue';
+import { ref, onMounted, onBeforeUnmount } from 'vue'
+import moment from 'moment'
+
+
+const time = ref();
+// 获取当前时间
+const getTime = () => {
+  time.value = new Date();
+};
+var timer = 0;
+
+onMounted(() => {
+  timer = setInterval(() => {
+    //设置定时器
+    getTime(); //自定义事件
+  }, 1000);
+});
+
+onBeforeUnmount(() => {
+  clearInterval(timer); //清除定时器
+  timer = 0;
+});
+
+</script>
+
+<template>
+  <div class="bg-[#f6f6f6] w-full ">
+    <div class="container mx-auto py-4">
+      <div class="grid grid-rows-10 grid-cols-7 grid-flow-row gap-4">
+        <div class="p-3 bg-white row-span-1 col-span-1 shadow-sm">
+          <div class="flex gap-2 items-center mb-1">
+            <ClockCircleFilled />
+            <span class="text-xs">測試時間</span>
+          </div>
+          <div class="bg-[#efefef] p-1 w-full text-center font-bold">
+            {{ moment().format('HH:mm:SS') }}
+          </div>
+        </div>
+        <div class="p-3 bg-white row-span-1 col-span-1 shadow-sm">
+          <div class="flex gap-2 items-center mb-1">
+            <EnvironmentFilled />
+            <span class="text-xs">是否命中目標</span>
+          </div>
+          <div class="bg-[#efefef] p-1 w-full text-center font-bold">
+            F/R
+          </div>
+        </div>
+        <div class="p-3 bg-white row-span-1 col-span-1 shadow-sm">
+          <div class="flex gap-2 items-center mb-1">
+            <EyeFilled />
+            <span class="text-xs">眼睛瞄準時間</span>
+          </div>
+          <div class="bg-[#efefef] p-1 w-full text-center font-bold">
+            {{ moment().format('HH:mm:SS') }}
+          </div>
+        </div>
+
+        <div class="p-3 bg-white row-span-6 col-span-4 shadow-sm">
+          <div class="font-bold text-center">場景展示</div>
+          <div class="flex mx-auto justify-center gap-8 mt-2">
+            <div class="text-center">二維場景</div>
+            <div class="text-center">三維場景</div>
+          </div>
+          <div class="">
+            <div class="flex justify-center">
+              <video class="max-h-60 w-[50%]" controls muted src="@/assets/2/arm_body_2.mp4"></video>
+              <video class="max-h-60 w-[50%]" controls muted src="@/assets/2/arm_body_2.mp4"></video>
+            </div>
+            <div>
+              <video class="max-h-60 w-full" controls muted src="@/assets/2/arm_body_2.mp4"></video>
+            </div>
+          </div>
+        </div>
+
+        <div class="p-3 bg-white row-span-5 col-span-3 shadow-sm">
+          <div class="mb-3">
+            <img src="@/assets/2/001.jpg" alt="Scene">
+          </div>
+          <div class="flex justify-between">
+            <img class="w-[48%]" src="@/assets/2/001.jpg" alt="LineGraph">
+            <div class="w-[48%] flex justify-between">
+              <div class="flex w-[50%] h-full flex-col justify-around items-center">
+                <span>本次</span>
+                <span>上一次</span>
+                <span>下一次</span>
+              </div>
+              <div class="flex w-[50%] h-full flex-col justify-around items-center">
+                <div>
+                  <div>上臂與大臂的角度</div>
+                  <div class="bg-[#efefef] p-1 w-full text-center font-bold">90</div>
+                </div>
+                <div>
+                  <div>大臂與軀幹的角度</div>
+                  <div class="bg-[#efefef] p-1 w-full text-center font-bold">90</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="p-3 bg-white row-span-3 col-span-3 shadow-sm">
+          <div class="font-bold text-center mb-2">眼動數據</div>
+          <div class="flex justify-between">
+            <div class="w-[50%]">
+              <img src="@/assets/2/001.jpg" alt="Eyes">
+              <div class="bg-[#efefef] p-1">
+                <div class="font-bold">HEATMAP</div>
+                <div>內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容</div>
+              </div>
+            </div>
+            <div class="w-[40%]">
+              <img src="@/assets/2/001.jpg" alt="San">
+              <div class="bg-[#efefef] p-1">
+                <div class="font-bold">散點圖</div>
+                <div>內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容</div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div class="p-3 bg-white row-span-3 col-span-1 shadow-sm">
+          <div class="font-bold text-center mb-2">坐墊</div>
+          <div>
+            <video src="@/assets/2/arm_body_2.mp4"></video>
+            <div class="bg-[#efefef] p-1">
+              <div>內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容</div>
+            </div>
+          </div>
+        </div>
+        <div class="p-3 bg-white row-span-3 col-span-3 shadow-sm">
+          <div class="font-bold text-center mb-2">IMU</div>
+          <img src="@/assets/2/001.jpg" alt="San">
+        </div>
+
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
