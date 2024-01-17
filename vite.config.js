ab538e26315fc2d400f3a8bec8bad1a504c14f77
Lumen <lumen@slingshot.sh>
2024-01-17 15:08:32 +0800

    Fix

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const ThreeExamples = require('import-three-examples')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    ...ThreeExamples
   ],
   resolve: {
     alias: {
